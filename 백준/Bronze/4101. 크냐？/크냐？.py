e481cbf86723031762c3311d22ba6e2efdecf619
Bit Goeun <bitge.dev@gmail.com>
2025-05-31 00:29:21 +0900

    [Bronze V] Title: 크냐?, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/4101. 크냐？/크냐？.py
+++ 백준/Bronze/4101. 크냐？/크냐？.py
@@ -1,11 +1,5 @@
 while 1:
-    try:
-        a, b=map(int,input().split())
-        if a==0 and b==0:
-            break
-        if a>b:
-            print("Yes")
-        else:
-            print("No")
-    except EOFError:
+    a, b=map(int,input().split())
+    if a==0 and b==0:
         break
+    print("Yes" if a>b else "No")

680fd33bc192bc8b0b120df8f2f5b37d6409ab93
Bit Goeun <bitge.dev@gmail.com>
2025-06-19 23:52:42 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,6 +1,8 @@
 l=int(input())
 code=input()
 result=0
+mod = 1234567891
 for i in range(l):
-    result+=(ord(code[i])-96)*31**i
+    result+=(ord(code[i])-96)*pow(31, i, mod)
+    result%=mod
 print(result)
